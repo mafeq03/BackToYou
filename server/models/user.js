88cdc54d1316d6391b20ed8d4ac6916bbf9fb2c1
mafeq03 <mafeq03@gmail.com>
2018-08-27 21:37:36 -0400

    updated

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+  name: String,
+  email: String,  
+  username: String,
+  password: String,
+  location: String,
+  profilePic: {
+    type: String, 
+    default: 'http://wwwicon-art.info/wp-content/uploads/2017-cheshire-yin-yang-cats-etsy-portraits-of-delicate-cat-yin-yang.jpg',
+  },
+  facebookID: String,
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
