181d10b0b811be21827e87e4c276c962b4aff4ac
mafeq03 <mafeq03@gmail.com>
2018-09-02 18:16:52 -0400

    some debugging done

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ mongoose
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-const passportSetup = require('./configs/passport');
+const passportSetup = require('./config/passport');
 passportSetup(passport);
 
 
